64a655f8815528022d26a6569ed312089e9c31f8
Mario Antonioletti <mario@epcc.ed.ac.uk>
2022-06-14 12:28:34 +0100

    Playing with data and code.

--- Src/DataPlayGround.R
+++ Src/DataPlayGround.R
@@ -11,6 +11,7 @@
 
 # Load packages to be used
 library(readr, quietly = TRUE)
+library(tidyr)
 library(lubridate, quietly = TRUE, warn.conflicts = FALSE)
 library(dplyr, warn.conflicts = FALSE)
 library(ggplot2, quietly = TRUE)
@@ -339,6 +340,7 @@ topics_cols <- cols(
 )
 
 # Read the data
+# also from: https://gtr.ukri.org/resources/classificationlists.html
 topics <- read_csv(file = "../Data/topics.csv", col_types = topics_cols)
 
 # Grant categories - "Studentship" "Research Grant" "Training Grant" "Fellowship"
@@ -379,7 +381,138 @@ length(unique(subjects$grantReference)) # 7180
 nrow(topics) # 23708
 length(unique(topics$grantReference)) #11382
 
-## Data and Software Outputs -----------------------------------------------
+
+### Topics investigation ----
+
+# Categories to use
+categories <- c("Area Studies",
+                "Demography",
+                "Development studies",
+                "Economics",
+                "Education",
+                "Environmental planning",
+                "History",
+                "Human Geography",
+                "Law & legal studies",
+                "Linguistics",
+                "Management & business studies",
+                "Political science. & international studies",
+                "Psychology",
+                "Science and Technology Studies",
+                "Social anthropology",
+                "Social policy",
+                "Social work",
+                "Sociology",
+                "Tools, technologies & methods",
+                "Other")
+
+# Define topics column types  to read from file
+columns <- cols(
+  id = col_character(),
+  text = col_character(),
+  encodedText = col_character(),
+  percentage = col_character(),
+  status = col_character(),
+  grantReference = col_character(),
+  grantCategory = col_character()
+)
+
+# read in the topics data
+topics <- read_csv(file = "../Data/topics.csv", col_types = columns)
+
+# Add a number of topics assigned and a fraction contribution column to the data
+topics <- topics                                  %>%
+          group_by(grantReference)                %>%
+          mutate(Nsub = n())                      %>%
+          mutate(fracContrib = round(1/Nsub, 3))  %>%
+          rename(topic = text, encodedTopic = encodedText)
+
+# Plot topic distribution
+topics %>% ggplot(aes(x = Nsub)) + geom_bar() + theme_bw()
+
+# Join with the ESRC data
+esrc_topics <- esrcdat %>%
+               left_join(topics, by = c("ProjectReference" = "grantReference")) %>%
+               select( -percentage, -status, -grantCategory)
+
+# Provide values for Nsub (1) and the fracContrib (1) when there is no subject
+esrc_topics$Nsub[is.na(esrc_topics$topic)] <- 1
+esrc_topics$fracContrib[is.na(esrc_topics$topic)] <- 1
+
+# Create new column
+esrc_topics$category <- rep("-",nrow(esrc_topics))
+
+# Set an uncategorised label
+esrc_topics$category[is.na(esrc_topics$topic)] <- "Uncategorised"
+
+
+# Function to select the right entries and reduce typing - only assign topics that have not been assigned yet
+sel <- function(parseString){
+  return(grepl(parseString, esrc_topics$topic, ignore.case = TRUE) &
+         (esrc_topics$category == "-") )
+}
+
+# assign the categories based on the topics
+esrc_topics$category[sel("area")] <- "Area Studies"
+# This also takes Demography and human geography, the latter being another category
+esrc_topics$category[sel("demography|popul|geront")] <- "Demography"
+esrc_topics$category[sel("development studies|devel")] <- "Development studies"
+esrc_topics$category[sel("economics|econo|employ|admin")] <- "Economics"
+esrc_topics$category[sel("education")] <-  "Education"
+esrc_topics$category[sel("environmental planning|environ|rural|ecology|wind|ocean|water|crop|animal|tox|weather|hydro|solar|carbon|lives|exper|plant|scale|waste|earth")] <- "Environmental planning"
+esrc_topics$category[sel("history")] <- "History"
+esrc_topics$category[sel("\\bgeography|urban|carto|spatial|geohaz")] <- "Human Geography"
+esrc_topics$category[sel("law|crimin|legal|ethics")] <- "Law & legal studies"
+esrc_topics$category[sel("languages|linguistics|phonetic|^language|lingui|epist|semant|lex|syntax|translation|morph")] <- "Linguistics"
+esrc_topics$category[sel("management|business|r&d|finance|leader|entrep|industrial|organ")] <- "Management & business studies"
+esrc_topics$category[sel("pol\\.")] <- "Political science. & international studies"
+esrc_topics$category[sel("politic|international")] <- "Political science. & international studies"
+esrc_topics$category[sel("america|africa|asiatic|european|middle|peace|colonial|conflict|war|religion|intern|elections|govern|rights")] <- "Political science. & international studies"
+esrc_topics$category[sel("women")] <- "Political science. & international studies"
+esrc_topics$category[sel("psychology|psych")] <- "Psychology"
+esrc_topics$category[sel("^science")] <- "Science and Technology Studies"
+esrc_topics$category[sel("anthropology|ethnic")] <- "Social anthropology"
+esrc_topics$category[sel("policy")] <- "Social policy"
+esrc_topics$category[sel("\\bwork")] <- "Social work"
+esrc_topics$category[sel("sociology|social")] <- "Sociology"
+esrc_topics$category[sel("tools|instrument|measurement|method|statistics|secur|ict|numerical|comput|math|intelli|emerg|robot")] <- "Tools, technologies & methods"
+# Things that do not seem to fit any of the above categories
+esrc_topics$category[sel(paste0("rcuk|astro|dance|music|gene|museum|art\\b|cultural|digital|compos|",
+                                  "theology|science(s)?$|engineering$|drama|network|secular|acoust|aest|",
+                                  "philo|arch|^omic|bio|info|class|child|sport|tourism|computing|arts|strat|innov|",
+                                  "atmos|design|vis|med|^cat|mar|ener|health|sound|literature|immun|choreo|",
+                                  "manu|materi|clim|poll|terr|civ|food|sex|disease|agric|survey|journa|project|",
+                                  "photo|budd|surf|complex|micro|publish|celtic|genomic|writing|musc|cells|prot|",
+                                  "drug|control|optical|theat|safe|faith|organelles|metabol|meteo|library|mineral|islam")
+)] <- "Other"
+
+unique(esrc_topics$topic[sel("strat")])
+
+# Check all have been assigned
+if(any(esrc_topics$category == "-")) {
+  warning("There are some unassigned categories (",length(unique(esrc_topics$topic[esrc_topics$category == "-"])),") :",
+          paste(unique(esrc_topics$topic[esrc_topics$category == "-"]), sep = ", "))
+}
+
+# Generate stats for the categories
+esrc_topics %>% select(category, AwardPounds, fracContrib)        %>%
+  group_by(category)                                              %>%
+  summarise(Contribution = round(sum(fracContrib),1),
+            Amount = sum(fracContrib*AwardPounds))                %>%
+  mutate(totContrib = sum(Contribution), totAmount = sum(Amount)) %>%
+  mutate(percContrib = round(Contribution/totContrib,2)*100,
+         perAward = round(Amount/totAmount,2) * 100)              %>%
+
+  select(-totContrib, -totAmount)                                 %>%
+  relocate(percContrib, .after = Contribution)                    %>%
+  relocate(perAward, .after = Amount)                             %>%
+  mutate(across(where(is.numeric), ~ format(., big.mark = ",")))  %>%
+  arrange(desc(Amount))                                           %>%
+  kable(format="pipe", align= rep("l",5),
+        col.names = c("Category", "Number of awards","Number %", "Award (£)",
+                      "Award %"))
+
+?## Data and Software Outputs -----------------------------------------------
 
 # Read the software outputs data
 softouts_cols = cols(
@@ -414,3 +547,221 @@ dataouts_cols <- cols(
 )
 dataouts <- read_csv(file = "../Data/dataoutputs.csv",
                      col_types = dataouts_cols)
+
+
+# Funding across RCs ------------------------------------------------------
+
+# Trying to do a cross table to see funding across RCs.
+gtrdat %>% select(FundingOrgName, PIId, StartDate)           %>%
+           filter(!is.na(PIId) & year(StartDate) >= 2008)    %>%
+           select(-StartDate)                                %>%
+           group_by(FundingOrgName, PIId)                    %>%
+           tally()                                           %>%
+           pivot_wider(names_from = FundingOrgName, values_from = n, values_fill = 0, values_fn = sum) -> a
+
+# rows (PIIds) that have more than one non-zero value, i.e. funded by more than one RC
+a[apply(a[,2:ncol(a)] > 0, 1, sum) > 1,]
+
+gtrdat %>% select(FundingOrgName, PIId, StartDate)  %>%
+  filter(!is.na(PIId) & year(StartDate) >= 2008)    %>%
+  select(-StartDate)                                %>%
+  group_by(FundingOrgName, PIId)                    %>%
+  tally()                                           %>%
+  mutate(fc = FundingOrgName)                       %>%
+  pivot_wider(names_from = FundingOrgName, values_from = n, values_fill = 0, values_fn = sum) -> b
+
+gtrdat %>% select(FundingOrgName, PIId, StartDate)  %>%
+  filter(!is.na(PIId) & year(StartDate) >= 2008)    %>%
+  select(-StartDate)                                %>%
+  group_by(FundingOrgName, PIId)                    %>%
+  tally()                                           %>%
+  mutate(fc = FundingOrgName)                       %>%
+  pivot_wider(names_from = FundingOrgName, values_from = n, values_fill = 0, values_fn = sum)
+
+b %>% select(-PIId) %>% group_by(fc) %>% summarise_at(.vars = names(b)[3:11], .funs = colSums(.))
+
+b %>% group_by(fc, PIId) %>% summarise(across(where(is.numeric), ~ sum(.)))
+
+b %>% group_by(fc) %>% summarise(across(where(is.numeric),  ~ sum(.)))
+
+# Mapping data ----
+
+# https://datatricks.co.uk/creating-maps-in-r-2019
+# https://r-graph-gallery.com/168-load-a-shape-file-into-r.html
+# https://www.blog.cultureofinsight.com/2017/06/building-dot-density-maps-with-uk-census-data-in-r/
+
+library(leaflet)
+library(rgdal)
+library(rgeos)
+library(tidyr)
+library(broom)
+library(maptools)
+library(ggplot2)
+library(dplyr)
+
+# Countries: https://geoportal.statistics.gov.uk/datasets/ons::countries-december-2021-uk-bfc/explore?location=55.215430%2C-3.316939%2C6.64
+# Regions: https://geoportal.statistics.gov.uk/datasets/ons::regions-december-2020-en-bfc/explore?location=52.916342%2C-2.000000%2C7.61&showTable=true
+shapesfile <- readOGR(dsn="../Data/shapes")
+
+# tells you the max and min coordinates, the kind of projection in use
+summary(shapesfile)
+
+# how many regions you have
+length(shapesfile)
+
+# the first few rows of the data slot associated with the regions
+head(shapesfile@data)
+
+# The needs to be transformed using the tidy() function of the broom package.
+# The region argument of this function expect one of the column name if the
+# @data slot. It will be the region name in the new dataframe.
+mapdata <- tidy(shapesfile, region="RGN20NM")
+
+ggplot() + geom_polygon(data = mapdata, aes( x = long, y = lat, group = group), fill="#69b3a2", color="white") +
+            theme_void()
+
+# Check sample distributions ----
+
+
+# Use a two Sample Kolmogorov-Smirnov Test
+
+# Data from the GtR - institution and award numbers, rename some of the entries
+# to be compatible with the survey
+esrcdat %>% filter(Status == "Active")        %>%
+            select(Institution=LeadROName)    %>%
+            mutate(Institution = ifelse(Institution == "Queen Mary, University of London",
+                                        "Queen Mary University of London", Institution))   %>%
+            mutate(Institution = ifelse(Institution == "University of Abertay Dundee",
+                                        "Abertay University", Institution))                %>%
+            mutate(Institution = ifelse(Institution == "The James Hutton Institute",
+                                       "James Hutton Institute", Institution))             %>%
+            mutate(Institution = ifelse(Institution == "London School of Economics & Pol Sci", "LSE", Institution)) %>%
+            group_by(Institution)             %>%
+            tally()                           %>%
+            arrange(desc(n)) -> gtr
+
+# Data from the survey
+data %>% select(Institution = CleanLocs)     %>%
+         group_by(Institution)               %>%
+         filter(!is.na(Institution))         %>%
+         tally()                             %>%
+         arrange(desc(n)) -> sur
+
+
+# Set null values in the sample to zero
+gtr %>% left_join(sur, by = "Institution", suffix = c(".gtr",".sur")) %>%
+      mutate(n.sur = ifelse(is.na(n.sur), 0, n.sur)) -> gtr_sur
+
+# Plot the values, reduce the input by only having GtR values with more tha one count.
+gtr_sur %>% filter(n.gtr > 1) %>%
+  ggplot(aes(x=reorder(Institution, -n.gtr))) + theme_bw() +
+  geom_col(aes(y = n.gtr, fill = "green"), alpha = 0.25, colour = "black",  na.rm = TRUE) +
+  geom_col(aes(y = n.sur, fill = "red"), alpha = 0.25, colour = "black", na.rm = TRUE) +
+  theme(axis.text.x = element_text(angle= -90, hjust = 0, size = 6)) +
+  scale_y_log10(oob = squish_infinite) + ylab("Numbers") + labs(fill="Source") + xlab("Institutions") +
+  scale_fill_manual(labels = c("GtR", "Survey"), values = c("green","red"))
+
+# Do not have zero values - ignore null values in the join
+gtr %>% left_join(sur, by = "Institution", suffix = c(".gtr",".sur")) %>%
+  filter(!is.na(n.sur)) -> gtr_sur2
+
+gtr_sur2 %>% ggplot(aes(x=reorder(Institution, -n.gtr))) + theme_bw() +
+      geom_col(aes(y = n.gtr, fill = "green"), alpha = 0.25, colour = "black") +
+      geom_col(aes(y = n.sur, fill = "red"), alpha = 0.25, colour = "black") +
+      theme(axis.text.x = element_text(angle= -90, hjust = 0)) +
+      scale_y_log10() + ylab("Numbers") + xlab("Institution") + labs(fill="Source") +
+      scale_fill_manual(labels = c("GtR", "Survey"), values = c("green","red"))
+
+sur %>% left_join(gtr, by = "Institution", suffix = c(".sur",".gtr")) %>%
+      filter(!is.na(n.gtr)) -> sur_gtr
+
+
+sur_gtr %>% ggplot(aes(x=reorder(Institution, -n.gtr))) + theme_bw() +
+  geom_col(aes(y = n.gtr, fill = "green"), alpha = 0.25, colour = "black") +
+  geom_col(aes(y = n.sur, fill = "red"), alpha = 0.25, colour = "black") +
+  theme(axis.text.x = element_text(angle= -90, hjust = 0)) +
+  scale_y_log10() + ylab("Numbers") + xlab("Institution") + labs(fill="Source") +
+  scale_fill_manual(labels = c("GtR", "Survey"), values = c("green","red"))
+
+# Not sure if this is an appropriate test - require a continuous distribution
+# and the inputs are supposed to be cumulative density distribution.
+# https://www.statology.org/kolmogorov-smirnov-test-r/
+
+# Two sample Kolmogorov-Smirnov Test
+ks.test(gtr_sur[["n.gtr"]],gtr_sur[["n.sur"]])
+
+ks.test(gtr_sur2[["n.gtr"]],gtr_sur[["n.sur"]])
+
+ks.test(sur_gtr[["n.gtr"]],sur_gtr[["n.sur"]])
+
+# Compare results with the email list we used
+
+# The first email lists - cleaned
+pi_emails <- read_csv("../Data/Mailmerge_clean_list.csv", show_col_types = FALSE)
+
+# The second email list - not cleaned
+pi_emails2 <- read_csv("../Data/fname-sname-org-email.csv", show_col_types = FALSE)
+
+# The student data
+student_emails <- read_csv("../Data/esrc-students.csv", show_col_types = FALSE)
+
+# Vector of all the emails
+emails <- c(pi_emails[["Email"]], pi_emails2[["constructed email"]], student_emails[["constructed email"]])
+
+# Get the domains
+domains <- str_split(emails, "@", simplify = TRUE)[,2]
+
+# Get the institution names
+insts <- read_csv("/Users/mario/Git/gateway-to-research-playing-about/output/uni_and_email_address_construction.csv", show_col_types = FALSE)
+
+# Tally the results for emails sent out
+tibble(domain = domains) %>% left_join(insts, by = "domain")          %>%
+                             filter(!is.na(university))               %>%
+                             select(domain, Institution = university) %>%
+                             mutate(Institution = ifelse(Institution == "Queen Mary, University of London",
+                                                        "Queen Mary University of London", Institution))   %>%
+                             mutate(Institution = ifelse(Institution == "University of Abertay Dundee",
+                                                        "Abertay University", Institution))                %>%
+                             mutate(Institution = ifelse(Institution == "The James Hutton Institute",
+                                                        "James Hutton Institute", Institution))             %>%
+                             mutate(Institution = ifelse(Institution == "London School of Economics & Pol Sci", "LSE", Institution)) %>%
+                             group_by(Institution)                    %>%
+                             tally()                                  %>%
+                             arrange(desc(n)) -> ems
+
+# Survey data
+data %>% select(Institution = CleanLocs)  %>%
+         filter(!is.na(Institution))      %>%
+         group_by(Institution)            %>%
+         tally()                          %>%
+         arrange(desc(n)) -> sur2
+
+# Join the email data with the survey responses
+ems %>% left_join(sur2, by = "Institution", suffix = c(".ems",".sur")) %>%
+        mutate(n.sur = ifelse(is.na(n.sur), 0, n.sur)) -> ems_sur
+
+# Join the data but ignore null values
+ems %>% left_join(sur2, by = "Institution", suffix = c(".ems",".sur")) %>%
+  mutate(!is.na(n.sur)) -> ems_sur2
+
+# Plot the results
+ems_sur %>%
+  ggplot(aes(x=reorder(Institution, -n.ems))) + theme_bw() +
+  geom_col(aes(y = n.ems, fill = "blue"), alpha = 0.25, colour = "black") +
+  geom_col(aes(y = n.sur, fill = "red"), alpha = 0.25, colour = "black") +
+  theme(axis.text.x = element_text(angle= -90, hjust = 0)) +
+  scale_y_log10() + ylab("Numbers") + xlab("Institution") + labs(fill="Source") +
+  scale_fill_manual(labels = c("Emails", "Survey"), values = c("blue","red"))
+
+# Two sample Kolmogorov-Smirnov Test
+ks.test(ems_sur[["n.ems"]],ems_sur[["n.sur"]])
+
+# Emails numbers vs Award numbers (not quite counting the same things)
+gtr %>%  left_join(ems, by = "Institution", suffix = c(".gtr",".ems")) %>%
+         mutate(n.ems = ifelse(is.na(n.ems), 0, n.ems)) %>%
+         ggplot(aes(x=reorder(Institution, -n.ems))) + theme_bw() +
+         geom_col(aes(y = n.ems, fill = "blue"), alpha = 0.25, colour = "black") +
+         geom_col(aes(y = n.gtr, fill = "green"), alpha = 0.25, colour = "black") +
+         theme(axis.text.x = element_text(angle= -90, hjust = 0, size = 6)) +
+         scale_y_log10() + ylab("Numbers") + xlab("Institution") + labs(fill="Source") +
+         scale_fill_manual(labels = c("Gtr (awards)", "Emails"), values = c("green","blue"))
